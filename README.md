600adb134bc8c5607853f0f3a72e643fc440b5b7
Sam McLeod <samm@infoxchange.net.au>
2014-11-03 19:35:01 +1100

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-haveatoken
+getcitoken
 ==========
 
-Retrieves Gitlab-CI Token for Runner Authentication
+Retrieves Gitlab-CI Token for Automated Runner Authentication
+
+## Requirements
+
+- mechanize gem
+- yaml gem
+- ruby 1.9 or newer
+
+## Usage
+
+1) Edit config.yml to add in your username/password and gitlab-ci URL
+2) Run gettoken.rb
+3) Receive token and be forever happy
+
+## Tests
+
+hmmm... #TODO ?
---
+++ getcitoken.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+# getcitoken - retrieves gitlab-ci token for configuration of runners
+# Sam McLeod - https://github.com/sammcj/getcitoken
+
+require 'mechanize'
+require 'logger'
+require 'yaml'
+
+config = YAML.load_file('config.yml')
+gitlab_ci_url = config['gitlab_ci_url']
+
+agent = Mechanize.new
+agent.log = Logger.new "mechanize.log"
+
+login_page = agent.get gitlab_ci_url+"/user_sessions/new"
+login_form = login_page.form
+
+email_field = login_form.field_with(name: "user_session[email]")
+password_field = login_form.field_with(name: "user_session[password]")
+
+email_field.value = config['gitlab_username']
+password_field.value = config['gitlab_password']
+
+home_page = login_form.submit
+runner_page = agent.get agent.get gitlab_ci_url+"/admin/runners"
+
+# Return Token
+runner_page.search('code').each do |results|
+  puts results.to_s[6...-7] #TODO this is a bit(lot?) gross
+end
